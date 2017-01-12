4336e99e84de1afc1794fc5c0786db837d91f69b
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2017-01-12 21:56:00 -0200

    Spike element creation via delegate provider

--- src/test/kotlin/org/gradle/script/lang/kotlin/NamedDomainObjectContainerExtensionsTest.kt
+++ src/test/kotlin/org/gradle/script/lang/kotlin/NamedDomainObjectContainerExtensionsTest.kt
@@ -13,6 +13,8 @@ import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
 
 import org.junit.Test
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
 
 class NamedDomainObjectContainerExtensionsTest {
 
@@ -109,4 +111,88 @@ class NamedDomainObjectContainerExtensionsTest {
 
         verify(clean).delete("build")
     }
+
+    @Test
+    fun `can create new element via delegated property in monomorphic container`() {
+
+        val container = mock<NamedDomainObjectContainer<DomainObject>> {
+            on { create("domainObject") } doReturn DomainObject()
+        }
+
+        @Suppress("unused_variable")
+        val domainObject by container.factory
+
+        verify(container).create("domainObject")
+    }
+
+    @Test
+    fun `can create and configure new element via delegated property in monomorphic container`() {
+
+        val element = DomainObject()
+        val container = mock<NamedDomainObjectContainer<DomainObject>> {
+            on { create("domainObject") } doReturn element
+            on { getByName("domainObject") } doReturn element
+        }
+
+        val domainObject by container.factory {
+            foo = "domain-foo"
+            bar = true
+        }
+
+        verify(container).create("domainObject")
+        assertThat(
+            domainObject,
+            equalTo(DomainObject("domain-foo", true)))
+    }
+
+    @Test
+    fun `can create new element via delegated property in polymorphic container`() {
+
+        val element = DomainObjectBase.Foo()
+        val container = mock<PolymorphicDomainObjectContainer<DomainObjectBase>> {
+            on { create("domainObject", DomainObjectBase.Foo::class.java) } doReturn element
+            on { getByName("domainObject") } doReturn element
+        }
+
+        val domainObject by container.factory(type = DomainObjectBase.Foo::class) {
+            foo = "domain-foo"
+        }
+
+        verify(container).create("domainObject", DomainObjectBase.Foo::class.java)
+        assertThat(
+            domainObject.foo,
+            equalTo("domain-foo"))
+    }
+
+    val <T : Any> NamedDomainObjectContainer<T>.factory
+        get() = NamedDomainObjectContainerDelegateProvider(this, {})
+
+    fun <T : Any> NamedDomainObjectContainer<T>.factory(configuration: T.() -> Unit) =
+        NamedDomainObjectContainerDelegateProvider(this, configuration)
+
+    class NamedDomainObjectContainerDelegateProvider<T : Any>(
+        val container: NamedDomainObjectContainer<T>, val configuration: T.() -> Unit) {
+
+        operator fun provideDelegate(thisRef: Any?, property: KProperty<*>) =
+            container.apply {
+                create(property.name).apply(configuration)
+            }
+    }
+
+    inline fun <T : Any, reified U : T> PolymorphicDomainObjectContainer<T>.factory(type: KClass<U>, noinline configuration: U.() -> Unit) =
+        factory(type.java, configuration)
+
+    fun <T : Any, U : T> PolymorphicDomainObjectContainer<T>.factory(type: Class<U>, configuration: U.() -> Unit) =
+        PolymorphicDomainObjectContainerDelegateProvider(this, type, configuration)
+
+    class PolymorphicDomainObjectContainerDelegateProvider<T : Any, U : T>(
+        val container: PolymorphicDomainObjectContainer<T>, val type: Class<U>, val configuration: U.() -> Unit) {
+
+        @Suppress("unchecked_cast")
+        operator fun provideDelegate(thisRef: Any?, property: KProperty<*>) =
+            container.apply {
+                create(property.name, type).apply(configuration)
+            } as PolymorphicDomainObjectContainer<U>
+    }
 }
+
